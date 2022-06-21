01cb5f2ad105fa889ca20c8bf84b0320ba7049ec
Imran Hendley <imran.hendley@gmail.com>
2022-06-21 12:02:23 -0400

    fix imports

--- setup.py
+++ setup.py
@@ -2,18 +2,18 @@
 Treys: A pure Python poker hand evaluation library
 """
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 setup(
     name='treys',
-    version='0.1.7',
+    version='0.1.8',
     description='treys is a pure Python poker hand evaluation library',
     long_description=open('README.rst').read(),
     author='Will Drevo, Mark Saindon, Imran Hendley',
     url='https://github.com/ihendley/treys',
     license='MIT',
-    packages=['treys'],
+    packages=find_packages(include=['treys', 'treys.*']),
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
